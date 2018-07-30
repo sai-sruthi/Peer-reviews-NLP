d6f7b3eb064872ea4025ff1df484c8e0c712af4d
Balaji Radhakrishnan <balag59@gmail.com>
2018-07-30 15:25:23 -0400

    removed non-essential inference code

--- bilstm/bilstm_inference.py
+++ bilstm/bilstm_inference.py
@@ -25,26 +25,6 @@ y_cat = to_categorical(df['y_true'], num_classes=3)
 x_train,x_test,y_train,y_test = train_test_split(x,y_cat,test_size=0.25)
 word_index = tok.word_index
 
-#create a dictionary which stores embeddings for every word
-embeddings_index = {}
-f = open('data/glove.840B.300d.txt',encoding="utf8")
-for line in f:
-    values = line.split()
-    word = values[0]
-    try:
-        coefs = np.asarray(values[1:], dtype='float32')
-    except:
-        pass
-    embeddings_index[word] = coefs
-f.close()
-
-#create the embedding matrix mapping every index in the corpus to it's respective embedding_vector
-embedding_matrix = np.zeros((len(word_index) + 1, 300))
-for word, i in word_index.items():
-    embedding_vector = embeddings_index.get(word)
-    if embedding_vector is not None:
-        embedding_matrix[i] = embedding_vector
-
 model = load_model('model/model-12.hdf5')
 sentences = np.array(["Nothing is mentioned about automated tests."])
 test_sentence = tok.texts_to_sequences(sentences)
